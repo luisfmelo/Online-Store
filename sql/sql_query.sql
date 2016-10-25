f477f1c5cd86c26c6fcc2355bc90a31fbf7d2b47
luisfmelo <luismelo7@gmail.com>
2016-10-25 08:24:06 +0100

    sql tables

---
+++ sql/sql_query.sql
@@ -0,0 +1,41 @@
+CREATE TABLE categories(
+  id SERIAL PRIMARY KEY,
+  categoryName VARCHAR
+);
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  ref INTEGER NOT NULL,
+  title VARCHAR NOT NULL,
+  author INTEGER,
+  price NUMERIC(6,2) NOT NULL,
+  category INTEGER REFERENCES categories,
+  decription VARCHAR,
+  stock INTEGER DEFAULT 0
+);
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR NOT NULL,
+  password VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  phone VARCHAR,
+  address VARCHAR,
+  admin BOOLEAN
+);
+
+CREATE TABLE orders(
+  id SERIAL PRIMARY KEY,
+  ref INTEGER NOT NULL,
+  userId INTEGER NOT NULL REFERENCES users,
+  state VARCHAR DEFAULT 'PENDING',
+  orderDate timestamp DEFAULT now(),
+  deliveryDate timestamp
+);
+
+CREATE TABLE productsOrdered (
+  id SERIAL PRIMARY KEY,
+  bookId INTEGER NOT NULL REFERENCES books,
+  orderId INTEGER NOT NULL REFERENCES orders
+);
