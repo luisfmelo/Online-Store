6b0adf0a7f7ce169e24bbd046ba1e9567b34d696
luisfmelo <luismelo7@gmail.com>
2016-10-26 11:00:51 +0100

    added footer structure

--- public/pages/common/footer.php
+++ public/pages/common/footer.php
@@ -1,2 +1,10 @@
-  </body>
+<footer>
+  <div class="social">
+      <!--<a href="#" target="_blank"><img src="../images/facebook.png" alt="facebook" class="socialIcons"></a>
+      <a href="#" target="_blank"><img src="../images/linkedin.png" alt="linkedin" class="socialIcons"></a>
+      <a href="#" target="_blank"><img src="../images/academic.png" alt="FEUP" class="socialIcons"></a>-->
+  </div>
+  <small>Copyright 2016 &#169; Lídia Cerqueira &amp; Luís Melo - SIEM 2016/2017</small>
+</footer>
+</body>
 </html>
