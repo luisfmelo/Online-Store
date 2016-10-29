96de663cab683decf710aaa22c3924e015a388b1
lmbcerqueira94 <licer.94@gmail.com>
2016-10-29 14:52:23 +0100

    register.php with session variables

--- public/actions/users/register.php
+++ public/actions/users/register.php
@@ -1,28 +1,29 @@
 <?php
-  print_r("PAGINA DE REGISTO");
+
   include_once('../../database/users.php');
   include_once('../../config/init.php');
   
+  if (!isset($_POST['username']) || !isset($_POST['password']) ||
+     !isset($_POST['name']) || !isset($_POST['email']) ||
+	 !isset($_POST['phone']) || !isset($_POST['address']) ) {
+		$_SESSION['error_messages'][] = 'There are some fields missing';
+		$_SESSION['form_values'] = $_POST;
+		header("Location: $BASE_URL" . '/pages/users/new_regist.php');
+		exit;		
+  }
+  else{
+	  $_SESSION['username'] = $_POST['username'];
+  }
+  
   $username = $_POST['username'];
-  print_r("$username");
   $password = $_POST['password'];
   $name 	= $_POST['name'];
   $email 	= $_POST['email'];
   $phone 	= $_POST['phone'];
   $address 	= $_POST['address'];
-  
-  //~ if (!isset($_POST['username']) || !isset($_POST['password']) ||
-     //~ !isset($_POST['name']) || !isset($_POST['email']) ||
-	 //~ !isset($_POST['phone']) || !isset($_POST['adress']) ) {
-		//~ $_SESSION['error_messages'][] = 'There are some fields missing';
-		//~ $_SESSION['form_values'] = $_POST;
-		//~ header("Location: $BASE_URL" . '/pages/users/new_regist.php');
-		//~ exit;		
-  //~ }
 
   addNewUser($username, $password, $name, $email, $phone, $address);
   
-  //~ header("Location: $BASE_URL");
+  header("Location: $BASE_URL" . '/pages/books/list_all.php');
 
-  
 ?>
--- public/pages/users/new_regist.php
+++ public/pages/users/new_regist.php
@@ -6,13 +6,13 @@
 <form method="POST" action= "<?=$BASE_URL?>/actions/users/register.php"> 
 	<section id = "books">
 		
-		Username:			<input type = "text" name="username"	size = 30></input> <br>
-		Nome: 				<input type = "text" name="name"		size = 30></input> <br>
-		Email:				<input type = "text" name="email"		size = 30></input> <br>
-		Telefone:			<input type = "text" name="phone"		size = 30></input> <br>
-		Morada:				<input type = "text" name="address"		size = 30></input> <br>
-		Password:			<input type = "password" name="password"size = 30></input> <br>
-		Confirmar Password: <input type = "password" name="confirmPassword"  size = 30></input> <br>
+		Username:			<input type = "text"		name="username"			size = 30></input> <br>
+		Nome: 				<input type = "text"		name="name"				size = 30></input> <br>
+		Email:				<input type = "text"		name="email"			size = 30></input> <br>
+		Telefone:			<input type = "text"		name="phone"			size = 30></input> <br>
+		Morada:				<input type = "text"		name="address"			size = 30></input> <br>
+		Password:			<input type = "password"	name="password"			size = 30></input> <br>
+		Confirmar Password: <input type = "password"	name="confirmPassword"	size = 30></input> <br>
 		
 		<input type = "submit" name="cmdsubmit" value="OK"></input>
 		
