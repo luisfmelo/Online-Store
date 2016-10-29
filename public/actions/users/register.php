e11421dc26b53b2581effcf5435a46623c5c5cc6
lmbcerqueira94 <licer.94@gmail.com>
2016-10-29 12:49:13 +0100

    new_regist.php (pages) added; register.php (actions) changed; users.php (database) changed

--- public/actions/users/register.php
+++ public/actions/users/register.php
@@ -1,3 +1,28 @@
 <?php
   print_r("PAGINA DE REGISTO");
+  include_once('../../database/users.php');
+  include_once('../../config/init.php');
+  
+  $username = $_POST['username'];
+  print_r("$username");
+  $password = $_POST['password'];
+  $name 	= $_POST['name'];
+  $email 	= $_POST['email'];
+  $phone 	= $_POST['phone'];
+  $address 	= $_POST['address'];
+  
+  //~ if (!isset($_POST['username']) || !isset($_POST['password']) ||
+     //~ !isset($_POST['name']) || !isset($_POST['email']) ||
+	 //~ !isset($_POST['phone']) || !isset($_POST['adress']) ) {
+		//~ $_SESSION['error_messages'][] = 'There are some fields missing';
+		//~ $_SESSION['form_values'] = $_POST;
+		//~ header("Location: $BASE_URL" . '/pages/users/new_regist.php');
+		//~ exit;		
+  //~ }
+
+  addNewUser($username, $password, $name, $email, $phone, $address);
+  
+  //~ header("Location: $BASE_URL");
+
+  
 ?>
--- public/database/users.php
+++ public/database/users.php
@@ -22,5 +22,40 @@
 
     return $res['admin'] ? 1 : 0;
   }
+  
+  function addNewUser($user, $password, $name, $email, $phone, $address ){
+	global $conn;
+	$stmt = $conn->prepare ("INSERT INTO e_store.users
+							VALUES (DEFAULT, '$user', '$password', '$name', '$email', '$phone', '$address', true);"); 
+							
+	$stmt->execute();
+  }
+  
+  function removeUser($user){
+	global $conn;
+	$stmt = $conn->prepare ("DELETE FROM e_store.users 
+							 WHERE username = '$user';");
+							 
+	$stmt->execute();
+  }
+  
+  function editUser($user, $password, $name, $phone, $address, $email){
+	global $conn;
+	$stmt = $conn->prepare ("UPDATE e_store.users
+							 SET password='$password', name='$name', phone='$phone', address='$address', email='$email'
+							 WHERE username='$user';");
+						
+	$stmt->execute();
+  }
+  
+  function getUserByUsername($username){
+    global $conn;
+    $stmt = $conn->prepare("SELECT *
+                            FROM e_store.users
+							WHERE username = '$username';");
 
+    $stmt->execute();
+    return $stmt->fetchAll();
+  }
+    
 ?>
