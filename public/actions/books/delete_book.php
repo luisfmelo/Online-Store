ee41d2c9279ee499586d1a7c5c3652e5f5013d8a
luisfmelo <luismelo7@gmail.com>
2016-11-02 14:14:56 +0000

    merge complete

--- public/actions/books/delete_book.php
+++ public/actions/books/delete_book.php
@@ -2,10 +2,10 @@
 
   include_once('../../config/init.php');
   include_once($BASE_DIR .'/database/books.php');
-  
+
   $book_ref = $_GET['ref'];
   print_r($book_ref);
-  
+
   //~ deleteBook($book_id);
-	
+
 ?>
