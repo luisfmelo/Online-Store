9cbb3dd9c5b5077d8ba7b5c6ec11ca4a7e919ab4
luisfmelo <luismelo7@gmail.com>
2016-11-02 17:12:06 +0000

    minor changes

--- public/pages/orders/shopping_cart.php
+++ public/pages/orders/shopping_cart.php
@@ -13,33 +13,61 @@
     <h2 class="bigTitle">
       O meu Cesto
     </h2>
-    <section id="cartItems">
-      <article class="cartItem">
-        <div class="cartQtt">
-          <ul>
-            <li>+</li>
-            <li><input type="text" name="qnt" value="<?=$_SESSION['cart'][1]?>"></li>
-            <li>-</li>
-          </ul>
+    <!--<section id="cartItems">
+      <article class="cartItem row">
+        <div class='book-data'>
+          <span class='title'>Prevenção de Lesões no Desporto</span><br />
+          <span class='author'>Luís Horta</span><br />
+          <span class='cartQtt'>Quantidade: <input type="text" name="qnt" value="//$_SESSION['cart'][1]?>"></span><br />
+        </div>
+        <div class='addBtn'>
+          <span class='price'>€ 16.90</span><br />
+          <span class='soldOut'>
+            <small>Esgotado</small>
+          </span>
         </div>
       </article>
+    </section>-->
+    <section id="cartItems">
+      <?php
+      foreach ($books as $book) {
+        $cover =
+          file_exists($IMG_DIR . '/covers/' . $book['ref'] . '.png')      ?
+                            $IMG_DIR . '/covers/' . $book['ref'] . '.png' :
+                            $IMG_DIR . '/covers/default.png' ;
+
+        echo "<article class='cartItem'>";
+          echo "<img class='cover' src=" . $cover . " />";
+
+          echo "<div class='book-data'>";
+            echo "<span class='title'>" . $book['title'] . "</span><br />";
+            echo "<span class='author'>" . $book['author'] . "</span><br />";
+            echo "<span class='cartQtt'>Quantidade: <input type="text" name="qnt" value="$_SESSION['cart'][1]"></span><br />";
+
+          echo "</div>";
+
+          echo "<div class='addBtn'>";
+              echo "<span class='price'>€ " . $book['price'] . "</span><br />";
+            if ( $book['stock'] != 0 )
+                echo "<span class='inStock'>
+                        <small>Em Stock</small>
+                      </span>";
+            else
+                echo "<span class='soldOut'>
+                        <small>Esgotado</small>
+                      </span>";
+
+
+          echo "</div>";
+        echo "</article>";
+      }
+      ?>
     </section>
+
   </div>
 
 
-  <?php
-  /*foreach ($_SESSION['cart'] as $item) {
-    echo "<article class='cartItem'>";
-      echo "<div class='book-data'>";
-        echo "<img class='cover' src=" . $book['cover'] . " />";
-        echo "<span class='title'>" . $book['title'] . "</span>";
-        echo "<span class='price'>€ " . $book['price'] . "</span>";
-        if ( $_SESSION['username'] != '')
-          echo "<i class='fa fa-plus-circle' aria-hidden='true'></i>";
-      echo "</div>";
-    echo "</article>";
-  }*/
-  ?>
-</div>
+
+  </div>
 
 <?php include '../common/footer.php';?>
