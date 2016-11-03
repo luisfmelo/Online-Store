e1fa62ee35c99845a9beb0bb1fa0ce6af87b3a1a
lmbcerqueira94 <licer.94@gmail.com>
2016-11-03 08:56:15 +0000

    stock_management (not complete)

--- public/js/main.js
+++ public/js/main.js
@@ -13,13 +13,13 @@ for (var i = 0; i < elems.length; i++)
     data[params[0]] = params[1];
 }*/
 
-function sortTheBooksNow() {
+function sortTheBooksNow (){
     var selectedID = event.target.value;
     var url = window.location.href;
     var cleanURL = url.replace(/\?*sort=\w+&/g, "");
     var cleanURL = url.replace(/\&*sort=\w+&*/g, "");
 
-    console.log(cleanURL);
+console.log(cleanURL);
 
     if (cleanURL.substring(cleanURL.length - 3) == 'php')
         cleanURL += '?sort=' + selectedID;
@@ -28,56 +28,54 @@ function sortTheBooksNow() {
     window.location.assign(cleanURL);
 };
 
-function toggleSearchBar() {
-    var elem = document.getElementById("searchBar");
+function toggleSearchBar(){
+  var elem = document.getElementById("searchBar");
 
-    if (elem.style.visibility == "hidden") {
-        elem.style.width = "300px";
-        elem.style.visibility = "visible";
-        elem.focus();
-        document.getElementById("searchForm").classList.add("expanded");
-    } else {
-        elem.style.width = "1px";
-        elem.style.visibility = "hidden";
-        document.getElementById("searchForm").classList.remove("expanded");
-    }
+  if ( elem.style.visibility == "hidden" )
+  {
+    elem.style.width= "300px";
+    elem.style.visibility = "visible";
+    elem.focus();
+    document.getElementById("searchForm").classList.add("expanded");
+  }
+  else
+  {
+    elem.style.width= "1px";
+    elem.style.visibility = "hidden";
+    document.getElementById("searchForm").classList.remove("expanded");
+  }
 };
 
-function updateCart() {
-    var inputs, index;
-    var cart = document.getElementById('cart');
-    var url = "../../actions/orders/update_cart.php?";
-
-    inputs = cart.getElementsByTagName('input');
-
-    for (index = 0; index < inputs.length; ++index)
-        if( Number(inputs[index].value) >= 0 )
-            url += inputs[index].name + "=" + inputs[index].value + '&';
-
-    if (url.charAt(url.length - 1) == '&')
-        url = url.substring(0, url.length - 1);
-
-    window.location.assign(url);
-};
-
-function deleteItem(ref, nome) {
-
-    var r = confirm("Tem a certeza que pretende remover " + nome + " do Carrinho de Compras?");
-    if (r)
-        window.location.assign("../../actions/orders/delete_book.php?ref=" + ref);
-};
 
 function deleteBookAlert(book_ref) {
-
-    var r = confirm("Tem a certeza que pretende remover o livro com a referência " + book_ref + "?");
-    if (r)
-        window.location.assign("../../actions/books/delete_book.php?ref=" + book_ref);
+    
+    var r = confirm("Tem a certeza que pretende remover o livro com a referência " + book_ref +  "?" );
+	if (r == true) {
+		window.location.assign("../../actions/books/delete_book.php?ref=" + book_ref);
+	} 
 }
 
 function deleteCustomerAlert(username) {
-
-    var r = confirm("Tem a certeza que pretende remover o cliente com a referência " + username + "?");
-    if (r == true) {
-        window.location.assign("../../actions/users/eliminate_account.php?ref=" + username);
-    }
+    
+    var r = confirm("Tem a certeza que pretende remover o cliente com a referência " + username +  "?" );
+	if (r == true) {
+		window.location.assign("../../actions/users/eliminate_account.php?ref=" + username);
+	} 
 }
+
+//~ function checkNewBook(title, author, category, price, description, stock){
+	
+	//~ if ( (title.length < 1) OR (author.lenght < 1) )
+		//~ alert("Title and Author must have more than one character");
+		
+	//~ if (stock < 0)
+		//~ alert("Check stock - it must be greater or equal to 0");
+	
+	//~ price = price.replace(/,/g, '.');
+		
+	//~ if (price < 0)
+		//~ alert("Check price - it must be greater or equal to 0");
+	
+//~ }	
+
+
