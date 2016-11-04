fc3e6f97ad7d8b6c74ab52db906e541bce6fe3fc
luisfmelo <luismelo7@gmail.com>
2016-11-04 09:14:56 +0000

    database/books.php arranjda

--- public/database/books.php
+++ public/database/books.php
@@ -1,4 +1,5 @@
 <?php
+/* Retorna num array todos os livros bem como suas informações */
   function getAllBooks($search, $order ) {
     global $conn;
     $query =           'SELECT *
@@ -16,106 +17,105 @@
     else if ($order == 'price_c')
       $query = $query . " ORDER BY e_store.books.price ASC";
     else
-      $query = $query . " ORDER BY e_store.books.title ASC";
-
-
-
-      //"OR e_store.books.author ILIKE '%" . $search . "%'""
+      $query = $query . " ORDER BY e_store.books.ref ASC";
 
     $stmt = $conn->prepare($query);
     $stmt->execute();
     return $stmt->fetchAll();
   }
-
+/* Recebe array com a informação de apens alguns livros -> páginas */
   function getSomeBooks($limit, $offset){
 	global $conn;
-	$query =           "SELECT *
-                        FROM e_store.categories
-                        INNER JOIN e_store.books
-                        ON e_store.categories.id = e_store.books.category
-                        ORDER BY books.ref ASC
-                        LIMIT '$limit' OFFSET '$offset';";
-
-    //~ $query =           "SELECT *
-                        //~ FROM e_store.books
-                        //~ ORDER BY ref ASC
-                        //~ LIMIT '$limit' OFFSET '$offset';";
+	$query = "SELECT *
+            FROM e_store.categories
+            INNER JOIN e_store.books
+            ON e_store.categories.id = e_store.books.category
+            ORDER BY books.ref ASC
+            LIMIT '$limit' OFFSET '$offset';";
 
     $stmt = $conn->prepare($query);
     $stmt->execute();
     return $stmt->fetchAll();
    }
 
+/* Recebe Array com informação de todos os livros de uma dada categoria */
   function getBooksByCategory($ref, $order) {
     global $conn;
     $query = "SELECT *
-              FROM e_store.categories
-              INNER JOIN e_store.books
-              ON e_store.categories.id = e_store.books.category
-              WHERE e_store.categories.ref = '$ref'";
+                        FROM e_store.categories
+                        INNER JOIN e_store.books
+                        ON e_store.categories.id = e_store.books.category
+                        WHERE e_store.categories.ref = '$ref' ";
 
     if ($order == 'name_a')
-      $query = $query . " ORDER BY e_store.books.title ASC";
+      $query = $query . "ORDER BY e_store.books.title ASC";
     else if ($order == 'name_z')
-      $query = $query . " ORDER BY e_store.books.title DESC";
+      $query = $query . "ORDER BY e_store.books.title DESC";
     else if ($order == 'price_d')
-      $query = $query . " ORDER BY e_store.books.price DESC";
+      $query = $query . "ORDER BY e_store.books.price DESC";
     else if ($order == 'price_c')
-      $query = $query . " ORDER BY e_store.books.price ASC";
+      $query = $query . "ORDER BY e_store.books.price ASC";
 
     $stmt = $conn->prepare($query);
     $stmt->execute();
     return $stmt->fetchAll();
   }
 
+/* Retorna todas as categorias existentes */
   function getBookCategories() {
     global $conn;
     $stmt = $conn->prepare('SELECT *
                             FROM e_store.categories');
-                            //ORDER BY time DESC');
     $stmt->execute();
     return $stmt->fetchAll();
   }
 
+/* Retorna o numero de livros existentes na BD */
   function getNoBooks() {
-	global $conn;
-    $stmt = $conn->prepare('SELECT COUNT(id) FROM e_store.books;');
+    global $conn;
+    $query = "SELECT COUNT(id)
+              FROM e_store.books;";
 
+    $stmt = $conn->prepare($query);
     $stmt->execute();
     return $stmt->fetchAll();
   }
 
-    function deleteBook($ref) {
-	global $conn;
-	$query =           "DELETE FROM e_store.books
+/* Apaga o livro com a referencia dada */
+  function deleteBook($ref) {
+    global $conn;
+    $query = "DELETE FROM e_store.books
 							WHERE ref = '$ref';";
+
     $stmt = $conn->prepare($query);
-
-
     $stmt->execute();
   }
 
+/* Retorna o preço do livro com a referencia dada */
   function getBookPrice($ref){
-  global $conn;
-  $stmt = $conn->prepare("SELECT price
-                          FROM e_store.books
-                          WHERE ref = '" . $ref . "'");
-                          //ORDER BY time DESC');
-  $stmt->execute();
-  return $stmt->fetchAll();
+    global $conn;
+    $query = "SELECT price
+              FROM e_store.books
+              WHERE ref = '" . $ref . "'";
+
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll();
 }
 
+/* Recebe array com referencias dos lvros que estao no carrinho de compras
+   e retorna toda a informação necessária desses livros */
   function getSelectedBooks($cart)
   {
 	  global $conn;
 
 	  $query = "SELECT *
-				FROM e_store.books
-				WHERE ref in (";
+				      FROM e_store.books
+				      WHERE ref in (";
 
-	  foreach($cart as $key => $value) {
+    // mete referencias dos livros entre virgulas
+	  foreach($cart as $key => $value)
 		  $query = $query . "'" . $key . "',";
-      }
 
 	  $query = substr($query, 0, -1);
 	  $query = $query . ');';
@@ -124,37 +124,40 @@
 	  $stmt->execute();
 	  return $stmt->fetchAll();
 
-}
+  }
 
+/* Atualiza informações de um livro com uma dada referencia - preço e stock apenas */
   function updateBook($ref, $price, $stock){
-	global $conn;
+    global $conn;
 
     $query = "UPDATE e_store.books
               SET price='$price', stock='$stock'
               WHERE ref='$ref';";
-	$stmt = $conn->prepare ($query);
 
+	  $stmt = $conn->prepare ($query);
     $stmt->execute();
   }
 
+/* Adiciona um novo livro à Base de Dados */
   function addNewBook($ref, $title, $author, $price, $category, $description,  $stock){
   	global $conn;
     $query = "INSERT INTO e_store.books
               VALUES (DEFAULT, '$ref', '$title', '$author', '$price', '$category', '$description', '$stock');";
-  	$stmt = $conn->prepare ($query);
 
+    $stmt = $conn->prepare ($query);
   	$stmt->execute();
   }
 
+/* Recebe o nome da categoria e retorna o seu id */
   function getCategoryId($categoryName){
-	 global $conn;
+	  global $conn;
 
-     $query = "SELECT e_store.categories.id
-			   FROM e_store.categories
-			   WHERE categoryName = '$categoryName';";
+    $query = "SELECT e_store.categories.id
+              FROM e_store.categories
+			        WHERE categoryName = '$categoryName';";
 
-	 $stmt = $conn->prepare($query);
-	 $stmt->execute();
-	 return $stmt->fetchAll();
+	  $stmt = $conn->prepare($query);
+	  $stmt->execute();
+	  return $stmt->fetchAll();
 	}
 ?>
