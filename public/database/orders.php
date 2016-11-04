b56b9643e2ab7d005c2513cfc9b5761609f37137
luisfmelo <luismelo7@gmail.com>
2016-11-04 09:31:03 +0000

    database/orders.php arranjda

--- public/database/orders.php
+++ public/database/orders.php
@@ -1,4 +1,6 @@
 <?php
+/* Recebe uma referencia gerada e o total da transação e
+   Adiciona uma nova encomenda ao utiliador atual da Sessão*/
   function new_Order($ref, $total){
     global $conn;
     $query = "INSERT INTO e_store.orders
@@ -14,11 +16,15 @@
     $stmt->execute();
   }
 
+/* Adiciona livros que estão na variavel de sessao do carrinho de compras
+   a uma encomenda efetuada com uma dada referencia */
   function add_Books_to_Order($ref){
     global $conn;
-    $query =          "INSERT INTO e_store.productsordered
-                       VALUES ";
+    $query = "INSERT INTO e_store.productsordered
+              VALUES ";
 
+
+// Percorre carrinho de compras e constroi query
     foreach ($_SESSION['cart'] as $k => $b) {
       if ( $k == "total" || $k == "checkout" )
         continue;
@@ -31,60 +37,67 @@
 
     $stmt = $conn->prepare($query);
     $stmt->execute();
-    
+
   }
 
+/* Faz update do stock aos produtos em carrinho */
   function updateStock(){
     global $conn;
-    $query = "";
 
-    foreach ($_SESSION['cart'] as $k => $b) {
-      isset($b['stock']) ? $s = 0 :
-                           $s = $b[0] - $b['stock'];
-
-      if ( $k == "total" || $k == "checkout" )
+    // cria uma query para atualizar o stock por cada produto
+    foreach ($_SESSION['cart'] as $key => $value) {
+      if ( $key == "total" || $key == "checkout" )
         continue;
-      $query = "UPDATE e_store.books
-                SET stock =" . $s . "
-                WHERE id = (select id from e_store.books where ref = '" . $k . "'); ";
+      // se para um produto do carrinho existir um campo de stock significa que
+      // a quantidade pedida é superior ao stock logo novo stock = 0
+      // caso contrario stock = stock anterior - quantidade encomendada
+      isset($value['stock']) ? $stck = 0 :
+                               $stck = $value[0] - $value['stock'];
 
+      $query = "UPDATE e_store.books
+                SET stock =" . $stck . "
+                WHERE id = (select id from e_store.books where ref = '" . $key . "'); ";
+
+      // adiciona query de um certo produto
       $stmt = $conn->prepare($query);
     }
 
     $stmt->execute();
   }
-  
+
+/* retorna todas as encomendas se utilizador é admin
+   Caso contrário, retorna as encomenda desse mesmo utilizador */
   function getOrdersByUsername($isAdmin, $username){
-	global $conn;
-	
-	if ($isAdmin == 1)
-		$query = "SELECT * 
-				FROM e_store.orders 
-				INNER JOIN e_store.users
-				ON e_store.orders.userid = e_store.users.id;";
-				
-	else 			
-		$query = "SELECT * 
-				FROM e_store.orders 
-				INNER JOIN e_store.users
-				ON e_store.orders.userid = e_store.users.id
-				INNER JOIN e_store.ordersstate
-				ON e_store.orders.id = e_store.ordersstate.id
-				WHERE username='$username';";
-				
-    
+	  global $conn;
+
+	  if ($isAdmin == 1)
+      $query = "SELECT *
+  				      FROM e_store.orders
+  				      INNER JOIN e_store.users
+  				      ON e_store.orders.userid = e_store.users.id;";
+
+    else
+		  $query = "SELECT *
+				        FROM e_store.orders
+				        INNER JOIN e_store.users
+				        ON e_store.orders.userid = e_store.users.id
+				        INNER JOIN e_store.ordersstate
+				        ON e_store.orders.id = e_store.ordersstate.id
+				        WHERE username='$username';";
+
     $stmt = $conn->prepare($query);
     $stmt->execute();
     return $stmt->fetchAll();
   }
-  
+
+/* Retorna os estados possiveis da encomendas */
   function getOrdersState() {
     global $conn;
-    $stmt = $conn->prepare('SELECT *
-                            FROM e_store.ordersstate');
+    $query = "SELECT *
+              FROM e_store.ordersstate";
 
+    $stmt = $conn->prepare($query);
     $stmt->execute();
     return $stmt->fetchAll();
   }
-
 ?>
