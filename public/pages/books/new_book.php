a17978fe73f6debd4ab6ee145ae4b05f1f8327e5
luisfmelo <luismelo7@gmail.com>
2016-11-04 11:53:55 +0000

    frontend almost complete

--- public/pages/books/new_book.php
+++ public/pages/books/new_book.php
@@ -6,34 +6,48 @@
 
 ?>
 
-<section id = "mainContent">
+<div class="row">
+  <?php   include '../common/left_menu.php';  ?>
 
-	<?php include '../common/left_menu.php'; ?>
+  <div class="rightContent">
+    <h2 class="bigTitle">
+      <span>Adicionar Livro</span>
+    </h2>
 
-	<section id = "content">
-		<form id="BookForm">
+    <section id = "content">
+      <div class="left">
+        <form id="newBook" class="myForms">
 
-			Titulo:		<input type = "text"	name="title"		</input> <br>
-			Autor:		<input type = "text"	name="author"		</input> <br>
-			Descrição:	<input type = "text"	name="description"	</input> <br>
-			
-			Categoria:
-			<Select name="category">
-				<?php
-				foreach ($categories as $category) 
-					echo "<Option value='" .$category['categoryname']. "'>" . $category['categoryname'] . "</Option> <br/>";
-				?>
-			</Select>
-			<br>
-			Preço: 		<input type = "text"	name="price"		</input> <br>
-			Stock: 		<input type = "text"	name="stock"		</input> <br>
-			
-			<input type = "button" onClick="NewBookCheck()" value="Adicionar"></input>
+    			Titulo:		<br />
+          <input type = "text"	name="title"/> <br />
+    			Autor:		<br />
+          <input type = "text"	name="author"/> <br />
+    			Descrição:		<br />
+          <textarea rows="6" cols="50" name="description"></textarea> <br />
 
-		</form>	
+    			Categoria: <br />
+          <Select name="category">
+    				<?php
+    				foreach ($categories as $category)
+    					echo "<Option value='" .$category['categoryname']. "'>" . $category['categoryname'] . "</Option> <br/>";
+    				?>
+    			</Select>
+    			<br>
+    			Preço:		<br />
+          <input type = "text"	name="price"		</input> <br>
+    			Stock:		<br />
+          <input type = "text"	name="stock"		</input> <br>
 
-	</section>
+    			<input type = "button" onClick="NewBookCheck()" value="Adicionar"></input>
 
-</section>
+    		</form>
+      </div>
+    </section>
 
+    <div class="messages" style="margin-bottom: 20px;">
+      <?php include_once('../common/cart_msgs.php'); ?>
+    </div>
+
+  </div>
+</div>
 <?php include '../common/footer.php';?>
