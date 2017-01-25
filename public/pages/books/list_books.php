b831b6549e2484f1c706d4209759c504bb8b1f90
Luís Melo <luismelo7@gmail.com>
2017-01-25 00:18:33 +0000

    fix category books display bug

--- public/pages/books/list_books.php
+++ public/pages/books/list_books.php
@@ -15,10 +15,8 @@
 
   /* obter livros de acordo com os "parâmetros" de pesquisa seleccionados pelo utilizador */
   if (isset($_GET['id'])){
-	   //$number_of_books = TotalNumberBooksByCategory($_GET['id'])[0]['count'];
-     //$books = listSomeBooksByCategory($_GET['id'], $_GET['sort'], $n_books_per_page, ($page-1) * $n_books_per_page);
-     print_r($_GET);
-
+	   $number_of_books = TotalNumberBooksByCategory($_GET['id'])[0]['count'];
+     $books = listSomeBooksByCategory($_GET['id'], $_GET['sort'], $n_books_per_page, ($page-1) * $n_books_per_page);
   }
   else {
 	   $number_of_books = TotalNumberSearchedBooks($_GET['search'])['count'];
